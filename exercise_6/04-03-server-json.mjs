0086c0bafde04327750c05395c21108920f4eee9
Massimo <rabuffo.massimo@gmail.com>
2023-11-16 20:22:38 +0100

    exercise_6

---
+++ exercise_6/04-03-server-json.mjs
@@ -0,0 +1,17 @@
+import { createServer } from "node:http";
+
+const server = createServer((request, response) => {
+  console.log("request received");
+
+  response.statusCode = 200;
+
+  response.setHeader("Content-Type", "application/json");
+
+  const jsonResponseBody = JSON.stringify({ location: "Mars" });
+
+  response.end(jsonResponseBody);
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
